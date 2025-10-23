ce3adf14abb6011d48efe0eee8821c83b37fad3b
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-23 08:12:36 +0700

    update code

--- src/components/RandomQuote.jsx
+++ src/components/RandomQuote.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { RefreshCw } from "lucide-react";
+import { RefreshCw, ShareIcon } from "lucide-react";
 
 export default function RandomQuote() {
   const [quote, setQuote] = useState({
@@ -39,8 +39,8 @@ export default function RandomQuote() {
   }, []);
 
   return (
-    <div className="bg-[#1E1E2E]/75 place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-xs shadow-lg shadow-[#1E1E2E] border border-white/15 m-4">
-      <h1 className="text-2xl md:text-3xl font-jaini-purva-regular mb-2 md:mb-4">Anime Quote</h1>
+    <div className="bg-[#1E1E2E]/75 place-self-center w-11/12 flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-xs shadow-lg shadow-[#1E1E2E] border border-white/15 m-4">
+      <h1 className="text-2xl md:text-5xl font-jaini-purva-regular mb-2 md:mb-4">Anime Quote</h1>
       <div className="mb-6">
         <svg className="w-6 md:w-8 text-white/40" fill="currentColor" viewBox="0 0 24 24">
           <path d="M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z"/>
@@ -50,7 +50,7 @@ export default function RandomQuote() {
         </p>
         {quote.character && (
           <div className="space-y-1 text-right">
-            <p className="text-md md:text-lg text-white/80 font-cherry-bomb-one-regular">
+            <p className="text-md md:text-lg text-white/75 font-chela-one-regular">
               ~ {quote.character}
             </p>
             {quote.anime && (
@@ -61,10 +61,18 @@ export default function RandomQuote() {
           </div>
         )}
       </div>
-      <button onClick={fetchQuote} disabled={loading} className="mt-4 flex items-center justify-center gap-2 bg-[#f06230]/75 hover:bg-[#f97c2c]/50 disabled:bg-[#f06230]/25 px-4 py-2 rounded-lg transition-all duration-200 border border-[#f06230]/75 hover:border-[#f97c2c]/50">
-        <RefreshCw className={`w-4 h-4 ${loading ? 'animate-spin' : ''}`} />
-        <span>{loading ? 'Loading...' : 'New Anime Quote'}</span>
-      </button>
+      <div className="flex justify-between items-center w-full">
+        <div className="tooltip tooltip-bottom" data-tip="New Quote">
+          <button onClick={fetchQuote} disabled={loading} className="mb-2 flex place-self-start gap-2 bg-[#f06230]/75 hover:bg-[#f97c2c]/50 disabled:bg-[#f06230]/25 px-4 py-2 rounded-lg transition-all duration-200 border border-[#f06230]/75 hover:border-[#f97c2c]/50">
+            <RefreshCw className={`w-4 ${loading ? 'animate-spin' : ''}`} />
+          </button>
+        </div>
+        <div className="tooltip tooltip-bottom" data-tip="Share">
+          <button onClick={fetchQuote} disabled={loading} className=" mb-2 flex place-self-start gap-2 bg-[#f06230]/75 hover:bg-[#f97c2c]/50 disabled:bg-[#f06230]/25 px-4 py-2 rounded-lg transition-all duration-200 border border-[#f06230]/75 hover:border-[#f97c2c]/50">
+            <ShareIcon className="" />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
