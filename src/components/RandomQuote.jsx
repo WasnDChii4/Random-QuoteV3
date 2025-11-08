f53fbc7dc8f3915279787b0bf4abb77931b792a3
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-11-08 13:40:58 +0700

    Update

--- src/components/RandomQuote.jsx
+++ src/components/RandomQuote.jsx
@@ -131,7 +131,7 @@ export default function RandomQuote() {
           </div>
           <div className="tooltip tooltip-bottom" data-tip="Copy">
             <button onClick={handleCopy} className=" mb-2 flex place-self-start gap-2 bg-[#f06230]/75 hover:bg-[#f97c2c]/50 disabled:bg-[#f06230]/25 px-4 py-2 rounded-lg transition-all duration-200 border border-[#f06230]/75 hover:border-[#f97c2c]/50">
-              <CopyIcon className="" />
+              <CopyIcon />
             </button>
           </div>
         </div>
